205d2495e08b2ca2055a6da4a03c80ea2c29cd56
LechCieslak <cieslak.lech@gmail.com>
2018-10-22 16:43:06 +0200

    dalsze prace: sekcja2 i rozpoczecie sekcji3

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var plumber = require('gulp-plumber');
+
+gulp.task('sass', function() {
+    gulp.src('scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+})
+
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['sass']);
+})
+
+gulp.task('default', ['sass', 'watch']);
